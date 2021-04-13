a4b18b635ddd27fac86e14b33ade07ab464dd9e4
5saviahv <5saviahv@users.noreply.github.com>
2021-04-13 21:07:20 +0300

    Split toString function - toJSON and toString functions

--- test/header.js
+++ test/header.js
@@ -5,8 +5,8 @@ describe("headers", () => {
     describe("main-header", () => {
         const mainHeader = require("../headers/mainHeader");
         // empty zip file
-        const empty = Buffer.from("504b0506000000000000000000000000000000000000","hex");
-        const readBuf = Buffer.from("504b050600000000cac0cefaed0b0000eeffc0000000","hex");
+        const empty = Buffer.from("504b0506000000000000000000000000000000000000", "hex");
+        const readBuf = Buffer.from("504b050600000000cac0cefaed0b0000eeffc0000000", "hex");
 
         // try read empty file
         it("read empty file", () => {
@@ -40,6 +40,15 @@ describe("headers", () => {
             expect(mainh.totalEntries).to.equal(0xface);
             expect(mainh.offset).to.equal(0xc0ffee);
             expect(mainh.size).to.equal(0xbed);
+
+            // test toJSON function
+            expect(mainh.toJSON()).to.eql({
+                diskEntries: 0xc0ca,
+                totalEntries: 0xface,
+                size: "3053 bytes",
+                offset: "0xC0FFEE",
+                commentLength: 0
+            });
         });
 
         it("set comment length", () => {
@@ -49,12 +58,30 @@ describe("headers", () => {
             expect(mainh.commentLength).to.equal(5);
             expect(mainh.mainHeaderSize).to.equal(22 + 5);
         });
+
+        // try read empty file
+        it("test toString function", () => {
+            const mainh = new mainHeader();
+            mainh.loadFromBinary(empty);
+
+            // test toJSON function
+            expect(mainh.toJSON()).to.eql({
+                totalEntries: 0,
+                size: "0 bytes",
+                offset: "0x0000",
+                diskEntries: 0,
+                commentLength: 0
+            });
+
+            // test toString function (remove CR from CRLF)
+            expect(mainh.toString().replace(/\r/g, "")).to.equal('{\n\t"diskEntries": 0,\n\t"totalEntries": 0,\n\t"size": "0 bytes",\n\t"offset": "0x0000",\n\t"commentLength": 0\n}');
+        });
     });
 
     describe("entry-header", () => {
         const entryHeader = require("../headers/entryHeader");
         const datestamp = [1981, 3, 1, 12, 10, 10];
-        const readBuf = Buffer.from("504b0102140014000008080045618102efbeadde0001000000020000000000000000000000000000000000000000","hex");
+        const readBuf = Buffer.from("504b0102140014000008080045618102efbeadde0001000000020000000000000000000000000000000000000000", "hex");
 
         // comparison values for readBuf
         const readBufValues = {
@@ -73,7 +100,7 @@ describe("headers", () => {
             diskNumStart: 0,
             commentLength: 0,
             extraLength: 0,
-            fileNameLength: 0,
+            fileNameLength: 0
         };
 
         it("compare binary header values with some predetermined values", () => {
@@ -88,14 +115,29 @@ describe("headers", () => {
             expect(head.entryHeaderSize).to.equal(46);
 
             // split into individual values by local time or timezone messes up our results
-            expect([
-                head.time.getFullYear(),
-                head.time.getMonth(),
-                head.time.getDate(),
-                head.time.getHours(),
-                head.time.getMinutes(),
-                head.time.getSeconds(),
-            ]).to.eql(datestamp);
+            expect([head.time.getFullYear(), head.time.getMonth(), head.time.getDate(), head.time.getHours(), head.time.getMinutes(), head.time.getSeconds()]).to.eql(datestamp);
+
+            // test toJSON function
+            const headerdata = {
+                made: 20,
+                version: 20,
+                flags: 2048,
+                method: "DEFLATED (8)",
+                crc: "0xDEADBEEF",
+                compressedSize: "256 bytes",
+                size: "512 bytes",
+                fileNameLength: "0 bytes",
+                extraLength: "0 bytes",
+                commentLength: "0 bytes",
+                diskNumStart: 0,
+                inAttr: 0,
+                attr: 0,
+                offset: 0,
+                entryHeaderSize: "46 bytes"
+            };
+
+            headerdata.time = head.time;
+            expect(head.toJSON()).to.eql(headerdata);
         });
 
         it("read binary and create new binary from it, they have to be equal", () => {
@@ -137,7 +179,7 @@ describe("headers", () => {
         });
 
         describe("data-header", () => {
-            const dataheader = Buffer.from("504b030414000008080045618102efbeadde000100000002000000000000","hex");
+            const dataheader = Buffer.from("504b030414000008080045618102efbeadde000100000002000000000000", "hex");
 
             const dataHeaderValues = {
                 compressedSize: 0x100,
@@ -147,7 +189,7 @@ describe("headers", () => {
                 fnameLen: 0,
                 method: 8,
                 size: 0x200,
-                version: 20,
+                version: 20
             };
 
             it("compare binary header values with predetermined values", () => {
